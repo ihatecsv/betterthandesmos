bc2249a526e874ce2e50345a8a84b87c9c5e5289
Drake <drakeluce@gmail.com>
2017-11-28 11:26:05 -0400

    Added option to disable secant-line rendering

--- btd.js
+++ btd.js
@@ -1,10 +1,11 @@
-//v1.06 BtD™
+//v1.07 BtD™
 
 var functionObjects = [ //Functions to graph
     {func: function(x){return Math.sin(x);}, color: color(255, 0, 0)},
     {func: function(x){return x*x;}, color: color(0, 0, 255)},
+    {func: function(x){return Math.pow(x, x);}, color: color(0, 255, 0)},
     {func: function(x){return x/(x-(-4));}, color: color(255, 179, 0)},
-    {func: function(x){return log(x);}, color: color(221, 0, 255)}
+    {func: function(x){return log(x);}, color: color(221, 0, 255)},
 ];
 
 var screenWidth = 400; //Width/height of display in pixels
@@ -14,7 +15,7 @@ var combineZoom = true; //Combined zoom or individual zoom
 var modifierZoom = 4; //Combined zoom
 var modifierZoomX = 4; //X zoom
 var modifierZoomY = 4; //Y zoom
-var plotScale = 1; //Dot density
+var plotScale = 0.1; //Dot density
 
 var axisStrokeWeight = 2; //X and Y axis stroke weight
 var graphStrokeWeight = 1; //Grid line stroke weight
@@ -57,10 +58,6 @@ strokeWeight(functionStrokeWeight);
 for(var k = -pixelMidPoint; k <= pixelMidPoint; k=k+plotScale){
     for(var i = 0; i < functionObjects.length; i++){
         stroke(functionObjects[i].color);
-        var x1 = k+pixelMidPoint;
-        var x2 = (k-plotScale)+pixelMidPoint;
-        var y1 = (-functionObjects[i].func(k*scaleX)*scaleY)+pixelMidPoint;
-        var y2 = (-functionObjects[i].func((k-plotScale)*scaleX)*scaleY)+pixelMidPoint;
-        line(x1, y1, x2, y2);
+        point(k+pixelMidPoint, (-functionObjects[i].func(k*scaleX)*scaleY)+pixelMidPoint);
     }
 }
